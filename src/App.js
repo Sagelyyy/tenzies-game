44a76bdb1fae4bb44f2cc73f2523e61d518a5c3b
Chris Weiskopf <orj.tsuk@gmail.com>
2022-02-11 20:13:02 -0500

    generating the random numbers for the dice, and initializing them into a state, them mapping them to the screen.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
+import React from "react";
 import "./App.css";
 import Title from "./components/Title";
 import Roll from "./components/Roll";
 import Die from "./components/Die";
 
 function App() {
+
+const [numbers, setNumbers] = React.useState(allNewDice())
+
+  function allNewDice(){
+    const dice=[]
+    for(let i=10; i>0; i-=1){
+      dice.push(Math.ceil(Math.random() * 6))
+    }
+    return dice
+  }
+
+  const diceElements = numbers.map((item, index) => {
+    return(
+      <Die key={index} value={item}/>
+    )
+  })
   return (
     <main className="App">
       <Title />
       <div className="game--container">
-        <Die value="10" />
-        <Die value="10" />
-        <Die value="10" />
-        <Die value="10" />
-        <Die value="10" />
-        <Die value="10" />
-        <Die value="10" />
-        <Die value="10" />
-        <Die value="10" />
-        <Die value="10" />
+      {diceElements}
       </div>
       <Roll />
     </main>
