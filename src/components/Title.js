c618d04b7f9699c7bbc4300dab7c9540e1b84bab
Chris <orj.tsuk@gmail.com>
2022-02-14 08:26:31 -0500

    initial push, getting basic layout working. no real logic yet.

---
+++ src/components/Title.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Title(){
+
+    return(
+        <div className="title--container">
+            <h1 className="title--title">Tenzies</h1>
+            <h5 className="title--desc">Roll until the dice are the same. Click
+            each die to freeze it at its current value
+            between rolls</h5>
+        </div>
+    )
+}
